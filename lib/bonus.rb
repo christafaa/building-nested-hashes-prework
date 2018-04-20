154070f0a81a02169973c6228802b87bbf6b6d43
Chris Alberti <christopherjalberti@gmail.com>
2018-04-20 04:20:54 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,8 +19,6 @@ def bonus
       ]
     }
   }
-
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 end
-
-bonus[:montague][:hero][:status] = "dead"
-bonus[:capulet][:heroine][:status] = "dead"
